3589ef2fdc76a937cd9bdab238730deacdc11127
Fábio Salata <fabio.salata@dartdigital.com.br>
2018-04-06 16:43:18 -0300

    Criação componente.

---
+++ RNPilotApp/src/screens/Register.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Container } from '../components/Container';
+
+class Register extends Component {
+  render() {
+    return (
+      <Container>
+        <View>
+          <Text> textInComponent </Text>
+        </View>
+      </Container>
+    );
+  }
+}
+
+export default Register;
